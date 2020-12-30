462e774ce6884d5764f28a07b70c02789f9bd810
pranavv481 <pranavv481@gmail.com>
2020-12-30 09:48:08 +0530

    materialui radio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  Container,
+  Box,
+  Radio,
+  Paper,
+  Typography,
+  RadioGroup,
+  FormControl,
+  FormControlLabel,
+  FormLabel
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import React, { useState } from 'react'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.grey[200],
+    width: "100vw",
+    height: "100vh",
+    paddingTop: theme.spacing(5)
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [gender, setGender] = useState("")
+  const handleChange = (event) => {
+    setGender(event.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className={classes.root}>
+      <Paper component={Box} width="30%" p={3} mx="auto">
+        <Box component="form">
+          <FormControl >
+            <FormLabel>Choose Your Gender</FormLabel>
+            <RadioGroup value={gender} onChange={handleChange} row>
+              <FormControlLabel value="female" control={<Radio />} label="Female" />
+              <FormControlLabel value="male" control={<Radio />} label="Male" />
+              <FormControlLabel value="other" control={<Radio />} label="Other" />
+             
+            </RadioGroup>
+          </FormControl>
+        </Box>
+      </Paper>
+    </Container>
   );
 }
 
